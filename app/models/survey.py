eeee5448bef301f86d063f4d289fb51099e6d3a0
MarcioDzn <marcio.roberto0739@gmail.com>
2026-07-12 18:14:23 -0300

    chore: add surveys table

---
+++ app/models/survey.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey, func
+from sqlalchemy.orm import relationship 
+from app.database import Base
+
+class Survey(Base):
+    __tablename__ = "surveys"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    title = Column(String(100), nullable=False)
+    description = Column(String(1000))
+    is_active = Column(Boolean, default=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    user = relationship("User", back_populates="surveys")
--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String, DateTime, Boolean, func
+from sqlalchemy.orm import relationship
 from app.database import Base 
 
 class User(Base):
@@ -9,3 +10,8 @@ class User(Base):
     email = Column(String(100), unique=True, nullable=False)
     password = Column(String(100), nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    surveys = relationship(
+        "Survey",
+        back_populates="user",
+        cascade="all, delete-orphan")
