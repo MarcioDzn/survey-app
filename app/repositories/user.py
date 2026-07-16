e3f2813259f9de414dce2a46f19f72b6b0c9a3a7
MarcioDzn <marcio.roberto0739@gmail.com>
2026-07-16 13:57:42 -0300

    feat: add user's repository and service mockup

---
+++ app/repositories/user.py
@@ -0,0 +1,13 @@
+class UserRepository:
+    
+
+    def __init__(self):
+        pass
+
+
+    def create(self):
+        pass
+
+
+    def get_all(self):
+        pass
---
+++ app/services/user.py
@@ -0,0 +1,14 @@
+class UserService:
+
+
+    def __init__(self):
+        pass
+
+
+    def create(self, user_data):
+        pass
+
+
+    def get_all(self):
+        pass
+
